67791b45e706f3bf4178d747668054fde92a33d4
Kapil Puranik <kapilpuranik764@gmail.com>
2026-01-03 14:57:52 +0530

    Created the second replica of the database

---
+++ database-replicas/second-node/internal/repository/balance_repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"second-node/internal/database"
+	"second-node/internal/model"
+)
+
+func Save(balance model.Balance) (model.Balance, error) {
+	err := database.DB.Create(&balance).Error
+	return balance, err
+}
+
+func GetById(id uint) (model.Balance, error) {
+	var balance model.Balance
+	err := database.DB.First(&balance, id).Error
+	return balance, err
+}
+
+func GetAll() ([]model.Balance, error) {
+	var balances []model.Balance
+	err := database.DB.Find(&balances).Error
+	return balances, err
+}
+
+func DeleteByID(id uint) error {
+	return database.DB.Delete(&model.Balance{}, id).Error
+}
